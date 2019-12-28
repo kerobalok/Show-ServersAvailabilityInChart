6b7bda4f96505ee0da9d0e531abffed9bf5f054f
Damian Sajdyk <damian2002.sajdyk@gmail.com>
2019-12-28 16:28:51 +0100

    Dodano podobiekt o nazwie timestamp przechowujacy date i czas wykonania pomiaru. Kolejnym krokiem jest przerobienie petli for w ktorej wykonywane sa pomiary pingow na petle while z warunkiem gwarantujacy nieskonczone dzialanie oraz opracowanie rotacji wykonywanych pomiarow, tzn. aby najstarsze byly kasowane, a nowe dopisywane do obiektow  i

--- test.ps1
+++ test.ps1
@@ -1,46 +1,46 @@
 # pętlę for z sekcji pingującej przerobić na while z warunkiem, który będzie zawsze fałszywy, tak aby pętla się zawsze wykonywała
+#sprawdzanie czy mamy do czynienia ze zmienną, tablicą, obiektem można sprawdzić poprzez $cos_do_sprawdzenia.gettype()
 <################################## -VARIABLES- #####################################>
 $interval_delay = 1
 <####################################################################################>
 
-#sprawdzanie czy mamy do czynienia ze zmienną, tablicą, obiektem można sprawdzić poprzez $cos_do_sprawdzenia.gettype()
 $file_content = (Get-Content "$PSScriptRoot\servers.txt") -notmatch '^#' 
 
 # create empty master object to store servers name with ping results
-$servers = New-Object PSObject -Property @{}
+$servers = New-Object  PSObject -Property ([ordered]@{})
 
-# for every server readed from file checking if object named as server name exist in main object, and if not creating one
+# creating subobjects corresponding to name servers and timestamp
 $file_content | ForEach-Object {
     $server_name = $_
     if( ($servers.PSObject.Properties | Select-Object -Expand Name) -match $server_name){
         write-host "istnieje" $server_name
     }
     else {
-        $servers | Add-Member -NotePropertyMembers @{$server_name=@()}
-    }
+        $servers | Add-Member -NotePropertyMembers ([ordered]@{$server_name=@()})
+    }  
 }
+$servers | Add-Member -NotePropertyMembers ([ordered]@{timestamp=@()})
 
-# doing ping measurements for every server
-for ($i=0; $i -lt 3; $i++){
-    foreach ($server in $servers.PSObject.Properties) {
-        $server_name = $server.Name
-        $ping = 0
-        
-        $ping = Get-CimInstance -Query "select * from win32_pingstatus where Address='$server_name'" | select-object -Expandproperty ResponseTime
-        
-        if ($NULL -eq $ping){
+
+for ($i=0; $i -le 2; $i++){
+    $timestamp = Get-Date -Format G
+    $servers.timestamp += $timestamp
+    
+    # doing ping measurements for every server
+    foreach ($server in $servers.PSObject.Properties) {    
+        $server_name = $server.Name # to jest problem bo pobiera każdy obiekt typu name i wyknuje na nim dalsze działania
+
+        if ($server_name -ne "timestamp"){
             $ping = 0
-        }
-
-        write-host $server_name ": " $ping
+            $ping = Get-CimInstance -Query "select * from win32_pingstatus where Address='$server_name'" | select-object -Expandproperty ResponseTime
         
-        #write-host $server_name":" $ping "ms"
-        $servers.$server_name += $ping
-    }
-    write-host "start przerwy"
-    Start-Sleep -Seconds $interval_delay
-    write-host "koniec przerwy"
-}
+            if ($NULL -eq $ping){
+                $ping = 0
+            }
 
-$servers
-#Remove-Variable $servers
+            write-host $server_name ":" $ping
+            $servers.$server_name += $ping
+        }
+    }
+    Start-Sleep -Seconds $interval_delay
+}
