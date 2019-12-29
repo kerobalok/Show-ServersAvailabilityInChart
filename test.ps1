7544d0339be1dff2a432f73eb5c11a37065b060d
Damian Sajdyk <damian2002.sajdyk@gmail.com>
2019-12-29 20:24:45 +0100

    Petle for zamieniona do while z warunkiem gwarantujacym jej nieskoczone dzialanie. Dodano rotowanie pomiarow w podobiektach przechowujacych wyniki pingow oraz podobiekcie timestamp.

--- test.ps1
+++ test.ps1
@@ -1,7 +1,10 @@
-# pętlę for z sekcji pingującej przerobić na while z warunkiem, który będzie zawsze fałszywy, tak aby pętla się zawsze wykonywała
+
 #sprawdzanie czy mamy do czynienia ze zmienną, tablicą, obiektem można sprawdzić poprzez $cos_do_sprawdzenia.gettype()
+# zmienic foreach ($server in $servers.PSObject.Properties) na $servers | Get-Member -MemberType NoteProperty | Select-Object -Expand Name    i zaraz potem w całym foreach zamienić zmienną $server_address na samą $server z warunku foreach
 <################################## -VARIABLES- #####################################>
-$interval_delay = 1
+$interval_delay = 1 #interval in seconds between finish and start another cycle of ping measurements.
+$FormatEnumerationLimit = 10 #for debuging purposes only. This variable sets number of item displayed on the screen when object is displayed. Default is 4. This variable in production environment can be easely deleted or commented.
+$number_measurements = 5
 <####################################################################################>
 
 $file_content = (Get-Content "$PSScriptRoot\servers.txt") -notmatch '^#' 
@@ -11,36 +14,43 @@ $servers = New-Object  PSObject -Property ([ordered]@{})
 
 # creating subobjects corresponding to name servers and timestamp
 $file_content | ForEach-Object {
-    $server_name = $_
-    if( ($servers.PSObject.Properties | Select-Object -Expand Name) -match $server_name){
-        write-host "istnieje" $server_name
+    $server_address = $_
+    if( ($servers.PSObject.Properties | Select-Object -Expand Name) -match $server_address){
+        write-host "istnieje" $server_address
     }
     else {
-        $servers | Add-Member -NotePropertyMembers ([ordered]@{$server_name=@()})
+        $servers | Add-Member -NotePropertyMembers ([ordered]@{$server_address=@()})
     }  
 }
 $servers | Add-Member -NotePropertyMembers ([ordered]@{timestamp=@()})
 
-
-for ($i=0; $i -le 2; $i++){
-    $timestamp = Get-Date -Format G
-    $servers.timestamp += $timestamp
+do {
     
-    # doing ping measurements for every server
+    # do ping measurements for every server
     foreach ($server in $servers.PSObject.Properties) {    
-        $server_name = $server.Name # to jest problem bo pobiera każdy obiekt typu name i wyknuje na nim dalsze działania
-
-        if ($server_name -ne "timestamp"){
-            $ping = 0
-            $ping = Get-CimInstance -Query "select * from win32_pingstatus where Address='$server_name'" | select-object -Expandproperty ResponseTime
+        $server_address = $server.Name # to jest problem bo pobiera każdy obiekt typu name i wyknuje na nim dalsze działania
         
+        if ($servers.$server_address.length -eq $number_measurements){
+            Write-Debug "usuwanie nadmiarowych elementow z tabicy"
+            $servers.$server_address = $servers.$server_address[1..$number_measurements]
+        }
+
+        # pomiary pingów wykonuj na wszystkich obiektach poza "timestamp"
+        if ($server_address -notmatch "timestamp"){
+            $ping = 0
+            $ping = Get-CimInstance -Query "select * from win32_pingstatus where Address='$server_address'" | select-object -Expandproperty ResponseTime
+            
             if ($NULL -eq $ping){
                 $ping = 0
             }
-
-            write-host $server_name ":" $ping
-            $servers.$server_name += $ping
+            $servers.$server_address += $ping
         }
     }
+
+    $timestamp = Get-Date -Format G
+    $servers.timestamp += $timestamp
+    
+    $servers | Format-Table -AutoSize
     Start-Sleep -Seconds $interval_delay
-}
+
+} while (1 -lt 2) #by regulating this condition you can set if ping measurements should be done forever or by regulated time. For forever measurements can be used condition (1 -lt 2)
