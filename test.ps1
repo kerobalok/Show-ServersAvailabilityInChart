de87c3e0418a63798d604d815aa318eaf2da6f44
Damian Sajdyk <damian2002.sajdyk@gmail.com>
2019-12-31 15:13:49 +0100

    Dodano mozliwosc uruchamiana skryptu z parametrami interval_delay i number_measurements. Jezeli skrypt zostanie uruchomiony bez podania parametrow, zostana wykorzystane domyslne wartosci tych parametrow, zapisane w skrypcie (aktualnie 1 i 3). Ponadto powrot do nazwy zmiennej server w petli foreach. W nastepnych krokach proba zaimplementowania tworzenia wykresow z wynikow pingow.

--- test.ps1
+++ test.ps1
@@ -1,11 +1,22 @@
-
 #sprawdzanie czy mamy do czynienia ze zmienną, tablicą, obiektem można sprawdzić poprzez $cos_do_sprawdzenia.gettype()
 
-<################################## -VARIABLES- #####################################>
-$interval_delay = 1 #interval in seconds between finish and start another cycle of ping measurements.
-$FormatEnumerationLimit = 10 #for debuging purposes only. This variable sets number of item displayed on the screen when object is displayed. Default is 4. This variable in production environment can be easely deleted or commented.
-$number_measurements = 5
+<################################# -PARAMETERS - ####################################>
+# Below are parameters with which script can be run. If script will be run without them, it will use they default values.
+param(
+    [Parameter(Mandatory=$false, ValueFromPipeline=$true)]
+    [ValidateRange(1,600)][int]$interval_delay = 1,
+    [ValidateRange(2,100)][int]$number_measurements = 3
+)
+
+write-host "interval_delay=" $interval_delay
+write-host "number_measurements=" $number_measurements
 <####################################################################################>
+
+
+<################################## -VARIABLES- #####################################>
+$FormatEnumerationLimit = 10 #for debuging purposes only. This variable sets number of item displayed on the screen when object is displayed. Default is 4. This variable in production environment can be easely deleted or commented.
+<####################################################################################>
+
 
 $file_content = (Get-Content "$PSScriptRoot\servers.txt") -notmatch '^#' 
 
@@ -14,43 +25,41 @@ $servers = New-Object  PSObject -Property ([ordered]@{})
 
 # creating subobjects corresponding to name servers and timestamp
 $file_content | ForEach-Object {
-    $server_address = $_
-    if( ($servers.PSObject.Properties | Select-Object -Expand Name) -match $server_address){
-        write-host "istnieje" $server_address
+    $server = $_
+    if( ($servers.PSObject.Properties | Select-Object -Expand Name) -match $server){
+        write-host "istnieje" $server
     }
     else {
-        $servers | Add-Member -NotePropertyMembers ([ordered]@{$server_address=@()})
+        $servers | Add-Member -NotePropertyMembers ([ordered]@{$server=@()})
     }  
 }
 $servers | Add-Member -NotePropertyMembers ([ordered]@{timestamp=@()})
 
 do {
-    
     # do ping measurements for every server
-    foreach ($server_address in $servers.PSObject.Properties.Name) {    
-        #$server_address = $server.Name # to jest problem bo pobiera każdy obiekt typu name i wyknuje na nim dalsze działania
-        
-        if ($servers.$server_address.length -eq $number_measurements){
+    foreach ($server in $servers.PSObject.Properties.Name) {    
+        if ($servers.$server.length -eq $number_measurements){
             Write-Debug "usuwanie nadmiarowych elementow z tabicy"
-            $servers.$server_address = $servers.$server_address[1..$number_measurements]
+            $servers.$server = $servers.$server[1..$number_measurements]
         }
 
         # pomiary pingów wykonuj na wszystkich obiektach poza "timestamp"
-        if ($server_address -notmatch "timestamp"){
-            $ping = 0
-            $ping = Get-CimInstance -Query "select * from win32_pingstatus where Address='$server_address'" | select-object -Expandproperty ResponseTime
+        if ($server -notmatch "timestamp"){
+            [int]$ping = 0
+            $ping = Get-CimInstance -Query "select * from win32_pingstatus where Address='$server'" | select-object -Expandproperty ResponseTime
             
             if ($NULL -eq $ping){
                 $ping = 0
             }
-            $servers.$server_address += $ping
+            $servers.$server += $ping
         }
     }
 
-    $timestamp = Get-Date -Format G
+    [datetime]$timestamp = Get-Date
     $servers.timestamp += $timestamp
     
-    $servers | Format-Table -AutoSize
+    $servers | Format-Table -AutoSize 
     Start-Sleep -Seconds $interval_delay
 
-} while (1 -lt 2) #by regulating this condition you can set if ping measurements should be done forever or by regulated time. For forever measurements can be used condition (1 -lt 2)
+} while (1 -lt 2) # by regulating this condition you can set if ping measurements should be done forever or by regulated time. 
+                  # For forever measurements can be used condition (1 -lt 2)
